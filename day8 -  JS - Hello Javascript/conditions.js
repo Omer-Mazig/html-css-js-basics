462620b54ccc7c8a2f68b31fe134da5bdd679513
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-03-07 13:38:18 +0200

    cond

--- day8 -  JS - Hello Javascript/conditions.js
+++ day8 -  JS - Hello Javascript/conditions.js
@@ -1,10 +1,25 @@
 // JS Conditionals
 // ctrl click to learn more 👉 https://www.w3schools.com/js/js_if_else.asp
 
-const age = 30
-
+// if & else statement with hard-coded condition (not really useful, but you get the idea)
 if (true) {
-    console.log('This will always run');
-}  else {
-    console.log('This will never run');
+  console.log("This will always run");
+} else {
+  console.log("This will never run");
+}
+
+// if & else statement with a variable condition
+const isHappy = true;
+if (isHappy) {
+  console.log("I am happy");
+} else {
+  console.log("I am not happy");
+}
+
+// if & else statement with a variable condition
+const age = 30;
+if (age > 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a minor");
 }
