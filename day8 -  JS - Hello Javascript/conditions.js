00ed3440712179e67ae9fe53fc8cb92fb156fff1
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-03-07 13:34:59 +0200

    cond

---
+++ day8 -  JS - Hello Javascript/conditions.js
@@ -0,0 +1,10 @@
+// JS Conditionals
+// ctrl click to learn more 👉 https://www.w3schools.com/js/js_if_else.asp
+
+const age = 30
+
+if (true) {
+    console.log('This will always run');
+}  else {
+    console.log('This will never run');
+}
