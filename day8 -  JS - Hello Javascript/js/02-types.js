c9ee4cbee84163752a60ed41feecd22ee0702a9b
Omer Mazig <omazig93@gmail.com>
2024-03-08 12:37:38 +0200

    loops basics

--- day8 -  JS - Hello Javascript/js/02-types.js
+++ day8 -  JS - Hello Javascript/js/02-types.js
@@ -1,9 +1,24 @@
 // JS Data Types:
 // ctrl click to learn more 👉 https://www.w3schools.com/js/js_datatypes.asp
 
+// data type has two categories:
+// 1. Primitive Data Types
+// 2. Object / Reference Data Types
+
+// PRIMARY DATA TYPES:
+
 // String:
 const firstName = "John";
 const lastName = "Doe";
+
+// we can get the length of a string using the length property
+console.log(firstName.length); // 4
+console.log(lastName.length); // 3
+
+// we cat get a specific character using the index
+console.log(firstName[0]); // J
+console.log(lastName[1]); // o
+console.log(lastName[5]); // undefined
 
 // Number:
 const age = 30;
@@ -18,6 +33,10 @@ const bike = undefined;
 
 // Null:
 const house = null;
+
+///////////////////////////////////////////////////////////////////
+
+// OBJECT / REFERENCE DATA TYPES: (more on this later)
 
 // Object:
 const person = {
