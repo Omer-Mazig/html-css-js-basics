9d37b47b2e10749a0e8927a6d7b46e51b6b77d04
Omer Mazig <omazig93@gmail.com>
2024-03-09 15:01:29 +0200

    arrays basics

--- day8 -  JS - Hello Javascript/js/11-objects.js
+++ day8 -  JS - Hello Javascript/js/11-objects.js
@@ -19,3 +19,39 @@ console.log("person.isMarried", person.isMarried); // false
 console.log("person (before increment)", person); // { name: 'John', age: 30, isMarried: false }
 person.age = person.age + 1; // increment the age by 1
 console.log("person (after increment)", person); // { name: 'John', age: 31, isMarried: false }
+
+// we can add new properties to the object
+person.city = "New York";
+console.log("person (after adding city)", person); // { name: 'John', age: 31, isMarried: false, city: 'New York' }
+
+// we can delete properties from the object
+delete person.city;
+console.log("person (after deleting city)", person); // { name: 'John', age: 31, isMarried: false }
+
+// we can also access the object properties using the bracket notation
+// this is usful in some cases. more on this later...
+console.log("person['name']", person["name"]); // John
+
+// we can add functions to the object. these are called methods
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2015,
+  drive: function () {
+    console.log("driving...");
+  },
+};
+
+car.drive(); // driving...
+
+// we can also add objects to the object
+let bankAccount = {
+  accountNumber: "123456",
+  balance: 1000,
+  owner: {
+    name: "Omer",
+    age: 30,
+  },
+};
+
+console.log("bankAccount.owner.name", bankAccount.owner.name); // Omer
